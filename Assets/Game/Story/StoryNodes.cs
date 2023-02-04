dd16fdefd6e2452fffb78a7311ee1f39d157efda
Mattia Belletti <mbelletti@greenteam.it>
2023-02-04 19:33:17 +0100

    .

--- Assets/Game/Story/StoryNodes.cs
+++ Assets/Game/Story/StoryNodes.cs
@@ -7,6 +7,14 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Anastomosi/Create Story")]
 public class StoryNodes : ScriptableObject
 {
+    [System.Serializable]
+    public enum ActionKind
+    {
+        Solve,
+        Coagula,
+        Amalgama
+    }
+
     [SerializeField] private TextAsset _inkTextAsset;
 
     private int _textHash = -1;
@@ -46,5 +54,29 @@ public class StoryNodes : ScriptableObject
             throw new System.Exception($"Expected 1 '{label}' tag for node {nodeName}, found {labelTags.Length}");
         }
         return labelTags[0];
+    }
+
+    public void ChooseStory(string nodeName, ActionKind actionKind)
+    {
+        story.ChoosePathString(nodeName);
+        story.Continue();
+        string tag;
+        switch (actionKind)
+        {
+            case ActionKind.Coagula: tag = "coagula"; break;
+            case ActionKind.Amalgama: tag = "amalgama"; break;
+            case ActionKind.Solve: tag = "solve"; break;
+            default: throw new System.Exception("Unknown tag");
+        }
+        foreach(var choice in story.currentChoices)
+        {
+            if(choice.tags.Contains(tag))
+            {
+                story.ChooseChoiceIndex(choice.index);
+                story.Continue();
+                return;
+            }
+        }
+        throw new System.Exception("cannot find choice with correct action");
     }
 }
--- Assets/Game/Story/Suggestion/SuggestionChoice.cs
+++ Assets/Game/Story/Suggestion/SuggestionChoice.cs
@@ -6,15 +6,22 @@ using UnityEngine;
 using UnityEngine.Assertions;
 using UnityEngine.EventSystems;
 
-public class SuggestionChoice : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class SuggestionChoice : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
     private TextMeshProUGUI _tmPro;
+
+    [SerializeField] private StoryNodes _storyNodes;
 
     [SerializeField] private ColorConstant _baseColor;
     [SerializeField] private ColorConstant _hoverColor;
 
+    private string _currentNode;
+
+    [SerializeField] private StoryNodes.ActionKind _actionKind;
+
     private void Start()
     {
+        Assert.IsNotNull(_storyNodes);
         _tmPro = GetComponentInChildren<TextMeshProUGUI>();
         Assert.IsNotNull(_tmPro);
         _tmPro.color = _baseColor.Value;
@@ -22,13 +29,22 @@ public class SuggestionChoice : MonoBehaviour, IPointerEnterHandler, IPointerExi
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        Debug.Log("On pointer enter");
         _tmPro.color = _hoverColor.Value;
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        Debug.Log("On pointer enter");
         _tmPro.color = _baseColor.Value;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        _storyNodes.ChooseStory(_currentNode, _actionKind);
+    }
+
+    public void OnNodeSelected(string nodeName)
+    {
+        Debug.Log("OnNodeSelected");
+        _currentNode = nodeName;
     }
 }
